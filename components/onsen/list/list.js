86417f72b5399348651b8ac5b9af6364b2d6d73b
Juha-Jarmo Heinonen <jammi@kuuhubb.com>
2021-06-24 12:45:09 +0300

    wip-lists improved

--- components/onsen/list/list.js
+++ components/onsen/list/list.js
@@ -1,8 +1,37 @@
 const {OnsenBase} = require('onsen/base');
+const ChevronListItem = require('onsen/listitemwithchevron');
 
 class OnsenList extends OnsenBase {
   get componentName() {
     return 'list';
+  }
+  get cellTagName() {
+    return 'ul';
+  }
+  draw() {
+    super.draw();
+    this.setCSSClass('list');
+  }
+  refreshList() {
+    if (this.listItems) {
+      this.listItems.forEach(listItem => listItem.die());
+    }
+    this.listItems = [];
+    let top = 0;
+    this.value.forEach(adminUser => {
+      const selected = adminUser.email === this.options.selectionValue.value.email;
+      this.listItems.push(
+        ChevronListItem.new([0, top, this.rect.width, 40], this, {value: adminUser, selected})
+      );
+      top += 44;
+    });
+  }
+  refreshValue() {
+    console.log('list refreshValue', this.value);
+    console.log('app value', this.app.adminAccountListValue.value);
+    if (this.isArray(this.value)) {
+      this.refreshList();
+    }
   }
 }
 
--- components/onsen/listitemwithchevron/listitemwithchevron.js
+++ components/onsen/listitemwithchevron/listitemwithchevron.js
@@ -4,6 +4,16 @@ class OnsenListItemWithChevron extends OnsenBase {
   get componentName() {
     return 'listitemwithchevron';
   }
+  get cellTagName() {
+    return 'li';
+  }
+  draw() {
+    super.draw();
+    this.setCSSClass('list__item');
+  }
+  setSelectedStyle(_state) {
+    this.toggleCSSClass(this.elemId, 'list__item--chevron', _state);
+  }
 }
 
 module.exports = OnsenListItemWithChevron;
